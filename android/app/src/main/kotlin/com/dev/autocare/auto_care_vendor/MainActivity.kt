632f977267126d25a5cfff11c6eb3754c5b06902
Morshed-islam <morshedislam21@gmail.com>
2024-10-20 10:48:02 +0600

    init setup

---
+++ android/app/src/main/kotlin/com/dev/autocare/auto_care_vendor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dev.autocare.auto_care_vendor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/constants/assets/app_images.dart
@@ -0,0 +1,7 @@
+import 'package:auto_care_vendor/core/extensions/extensions.dart';
+
+class AppImages {
+  ///png, jpej and jpg image path set
+  static String get placeholder => 'placeholder'.png;
+  static String get logo => 'logo'.png;
+}
